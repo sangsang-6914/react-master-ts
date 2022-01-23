31bb3b55a8a866165195b18ab60f3c2d02760731
sangsang-6914 <sangsang6914@gmail.com>
2022-01-23 18:30:36 +0900

    coin css add

---
+++ src/theme.ts
@@ -0,0 +1,10 @@
+import { DefaultTheme } from "styled-components"
+
+const theme:DefaultTheme = {
+    bgColor: "#192a56",
+    textColor: "#f5f6fa",
+    accentColor: "#00a8ff"
+}
+export {
+    theme
+}
